32b303e3c556844c0b6e6521d8ff672f2fb03e82
John Nguyen <johndvnguyen@gmail.com>
2018-07-06 10:42:11 -0700

    Heap_storage work in progress. Added Heapfil create, get_new, put

--- heap_storage.cpp
+++ heap_storage.cpp
@@ -12,6 +12,8 @@ using namespace std;
 
 typedef u_int16_t RecordID;
 typedef vector<RecordID> RecordIDs;
+static const uint BLOCK_SZ = 4096;
+
 
 //constructor
 SlottedPage::SlottedPage(Dbt &block, BlockID block_id, bool is_new) : DbBlock(block, block_id, is_new)
@@ -129,13 +131,6 @@ void SlottedPage::slide(u_int16_t start, u_int16_t end)
   }
 }
 
-// not sure what this stuff does yet
-//SlottedPage(const SlottedPage& other) = delete;
-//SlottedPage(SlottedPage&& temp) = delete;
-//SlottedPage& operator=(const SlottedPage& other) = delete;
-//SlottedPage& operator=(SlottedPage& temp) = delete;
-//
-
 //adds data as a new record to the slotted page block
 RecordID SlottedPage::add(const Dbt* data) throw(DbBlockNoRoomError)
 {
@@ -212,8 +207,122 @@ void SlottedPage::del(RecordID record_id)
 {
   u_int16_t size;
   u_int16_t loc;
+
   get_header(size,loc,record_id);
   put_header(record_id,0 ,0);
   //compact the records space
   slide(loc, loc + size);
 }
+
+//END SLOTTED page
+
+//START HeapFile
+//constructor might be complete in the header
+//HeapFile::HeapFile(string name) : DbFile(name), dbfilename(""), last(0), closed(true), db(_DB_ENV, 0)
+//{}
+
+//opens the berkeleyDB
+//int Db::open(DbTxn *txnid, const char *file,
+//    const char *database, DBTYPE type, u_int32_t flags, int mode);
+//DB class API docs https://web.stanford.edu/class/cs276a/projects/docs/berkeleydb/api_cxx/db_open.html
+void HeapFile::db_open(uint flags)
+{
+  //protected variable closed
+  if (!this->closed)
+  {
+    //The db is already open
+    return;
+  }
+  //create a db handle using db_create
+  DB* db_ptr= this->db.get_DB();
+  DB_ENV* dbenv_ptr = _DB_ENV->get_DB_ENV();
+  db_create(&db_ptr, dbenv_ptr, flags);
+  //Set the record length to the block size this is defined in storage_engine.h
+  this->db.set_re_len(BLOCK_SZ);
+  //set dbfilename
+  //this->dbfilename  = path(_DB_ENV + this->name + ".db");
+  //hardcoded for now
+  this->dbfilename  = "../Data/poop.db";
+  //python self.dbfilename = os.path.join(_DB_ENV, self.name + '.db')
+  //Null txnid is not transaction protected, 0 mode is default
+  this->db.open(nullptr ,this->dbfilename.c_str(),this->name.c_str(),DB_RECNO,(u_int32_t)flags,0);
+
+
+  // This creates a statistical structure and stores it in stat
+  // However stat does not appear to be a variable in the HeapFile class
+  //(void*) stat;
+  //this->db.stat(stat, DB_FAST_STAT);
+  //this->last = stat['ndata'];
+
+  this->closed = false;
+
+}
+
+//creates the physical Heapfile for DB
+void HeapFile::create(void)
+{
+  //need to open the DB
+  this->db_open(0U);
+  //create a new block
+  SlottedPage* block = this->get_new();
+  this->put(block);
+}
+
+//delete heapfile function
+void HeapFile::drop(void)
+{
+
+}
+
+//Open File
+void HeapFile::open(void)
+{
+  this->db_open(0U);
+}
+void HeapFile::close(void)
+{
+
+}
+//given in the assignment description
+//Allocates a block for the database files
+// Returns a SlottedPage that is empty for managing records
+SlottedPage* HeapFile::get_new(void)
+{
+  //char block[DB_BLOCK_SZ];
+  char block[BLOCK_SZ];
+  memset(block,0,sizeof(block));
+  Dbt data(block,sizeof(block));
+  //last is u_int32_t
+  int block_id= ++this->last;
+  Dbt key(&block_id, sizeof(block_id));
+
+    // write out an empty block and read it back in so Berkeley DB is managing the memory
+  SlottedPage* page = new SlottedPage(data, this->last, true);
+  // write it out with initialization applied
+  this->db.put(nullptr, &key, &data, 0);
+  this->db.get(nullptr, &key, &data, 0);
+  return page;
+}
+SlottedPage* HeapFile::get(BlockID block_id)
+{
+ return NULL;
+}
+
+
+
+//Handle for berkeleyDB Db::put
+//int Db::put(DbTxn *txnid, Dbt *key, Dbt *data, u_int32_t flags);
+void HeapFile::put(DbBlock* block)
+{
+  //get_block(),get_block_id are defined in storage_engine.h
+  BlockID block_id = block->get_block_id();
+  Dbt key(&block_id, sizeof(block_id));
+  this->db.put(nullptr, &key,block->get_block(),0);
+}
+
+BlockIDs* HeapFile::block_ids()
+{
+  return NULL;
+}
+
+//END HEAPFILE
